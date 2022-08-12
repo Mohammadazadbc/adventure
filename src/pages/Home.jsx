e046c5dc443f658e2b0b0b6317e85c29362719c0
mohammad azad <mohammadazad.bc@gmail.com>
2022-08-12 20:54:40 +0200

    cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "../App.css"
+import Card from '../componenet/card/Card'
 import HeroSection from '../componenet/Hero/HeroSection'
 function Home() {
   return (
     <>
         <HeroSection />
+        <Card />
     </>
   )
 }
